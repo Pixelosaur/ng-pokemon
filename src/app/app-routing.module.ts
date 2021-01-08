2f80b2b1976dc188de87da5fcd2dab712d153ca4
christina <xristitina@gmail.com>
2021-01-08 22:49:50 +0200

    Add app routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PokemonsComponent } from './pages/pokemons-container/pokemons.component';
+import { PokemonDetailsComponent } from './pages/pokemon-details-view/pokemon-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', redirectTo: 'pokedex', pathMatch: 'full' },
+    {
+        path: 'pokedex',
+        component: PokemonsComponent,
+    },
+    {
+        path: 'pokedex/:pokemon',
+        component: PokemonDetailsComponent,
+    },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

1f96694e91c22c2e4ac5a5b02055a335422c7ab1
christina <xristitina@gmail.com>
2021-01-10 13:45:02 +0200

    Modify blob shape

--- src/app/helpers/blob/blob.helper.ts
+++ src/app/helpers/blob/blob.helper.ts
@@ -1,10 +1,10 @@
 import * as _ from 'underscore';
 
-export function  generateBlob() {
-    const percentage1 = _.random(25, 75);
-    const percentage2 = _.random(25, 75);
-    const percentage3 = _.random(25, 75);
-    const percentage4 = _.random(25, 75);
+export function generateBlob() {
+    const percentage1 = _.random(20, 70);
+    const percentage2 = _.random(30, 70);
+    const percentage3 = _.random(30, 70);
+    const percentage4 = _.random(20, 60);
     const percentage11 = 100 - percentage1;
     const percentage21 = 100 - percentage2;
     const percentage31 = 100 - percentage3;

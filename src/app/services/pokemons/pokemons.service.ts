c2e7b55fd2ab59d6ddafc474943d0fb3453ffbe6
christina <xristitina@gmail.com>
2021-01-10 10:28:52 +0200

    Rename getPokemonList to getPokemons; modify and add types;

--- src/app/services/pokemons/pokemons.service.ts
+++ src/app/services/pokemons/pokemons.service.ts
@@ -1,8 +1,16 @@
+// Core
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { forkJoin, Observable } from 'rxjs';
+// Environments
 import { environment } from '../../../environments/environment';
+// RxJS
+import { forkJoin, Observable } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
+// Interfaces
+import { PokemonList } from '../../interfaces/pokemon-list.interface';
+import { PokemonDetail } from '../../interfaces/pokemon-detail.interface';
+import { Pokemons } from '../../interfaces/pokemons.interface';
+import { NamedApiResource } from '../../interfaces/named-api-resource.interface';
 
 @Injectable({
     providedIn: 'root',
@@ -10,7 +18,7 @@ import { map, mergeMap } from 'rxjs/operators';
 export class PokemonsService {
     constructor(private http: HttpClient) {}
 
-    getPokemonList(url: string | null = null): Observable<any> {
+    getPokemons(url: string | null = null): Observable<Pokemons> {
         const limit: string = '20';
         const offset: string = '0';
 
@@ -19,15 +27,20 @@ export class PokemonsService {
             : `${environment.serverUrl}/pokemon?limit=${limit}&offset=${offset}`;
 
         return this.http.get<any>(apiUrl).pipe(
-            mergeMap((response: any) =>
+            mergeMap((response: PokemonList) =>
                 forkJoin(
-                    ...response.results.map((res: any) => this.getPokemonDetails(res.url)),
+                    ...response.results.map((res: NamedApiResource) =>
+                        this.getPokemonDetails(res.url),
+                    ),
                 ).pipe(
-                    map((details) => {
-                        return {
+                    map((details: PokemonDetail[]) => {
+                        console.log(details);
+                        const pokemons: Pokemons = {
                             ...response,
                             details,
                         };
+
+                        return pokemons;
                     }),
                 ),
             ),

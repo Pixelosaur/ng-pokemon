e791a13ab5047b0d5ba1100bb7689eff75e7d0af
christina <xristitina@gmail.com>
2021-01-10 13:33:07 +0200

    Install underscore and create blob helper function

---
+++ src/app/helpers/blob/blob.helper.ts
@@ -0,0 +1,14 @@
+import * as _ from 'underscore';
+
+export function  generateBlob() {
+    const percentage1 = _.random(25, 75);
+    const percentage2 = _.random(25, 75);
+    const percentage3 = _.random(25, 75);
+    const percentage4 = _.random(25, 75);
+    const percentage11 = 100 - percentage1;
+    const percentage21 = 100 - percentage2;
+    const percentage31 = 100 - percentage3;
+    const percentage41 = 100 - percentage4;
+
+    return `${percentage1}% ${percentage11}% ${percentage21}% ${percentage2}% / ${percentage3}% ${percentage4}% ${percentage41}% ${percentage31}%`;
+}
